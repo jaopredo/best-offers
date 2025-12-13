4a47bdeecc4fbae276a04aee5bdfee7f24dfb003
jaopredo <joao.profissional.45@gmail.com>
2025-12-13 17:42:11 -0300

    Adaptando para fazer os testes e2e com configurações (env files) diferentes das configurações base

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { TypeOrmModule } from '@nestjs/typeorm'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 
 /* MODELS */
 import { User } from 'database/models/user'
@@ -12,19 +13,25 @@ import { Item } from 'database/models/item'
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'database',
-      port: 5432,
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DB,
-      entities: [ User, Font, Adapter, Category, Item ],
-      synchronize: true,
-      logging: false
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        username: config.get('DB_USER'),
+        password: config.get('DB_PASSWORD'),
+        database: config.get('DB_DB'),
+        entities: [ User, Font, Adapter, Category, Item ],
+        synchronize: true,
+        logging: false
+      })
   })
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [ AppController ],
+  providers: [ AppService ],
 })
 export class AppModule {}
