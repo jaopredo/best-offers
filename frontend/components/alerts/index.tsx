932d349b2fee972e2099e834b31e53bd13d362b3
jaopredo <joao.profissional.45@gmail.com>
2025-12-20 17:55:38 -0300

    Melhorando o design das mensagens ao serem exibidas (E adicionando responsividade)

--- frontend/components/alerts/index.tsx
+++ frontend/components/alerts/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useState, useEffect } from "react"
-import { motion } from "motion/react"
+import { motion, AnimatePresence } from "motion/react"
 
 import ErrorStoreManager from "@/alerts/errors"
 import SuccessStoreManager from "@/alerts/successes"
@@ -47,51 +47,67 @@ export default function Alerts() {
         }
     }, [])
 
-    return <>
-        <motion.div className={`fixed top-15 right-0 w-[30vw] rounded-sm p-2 flex flex-col gap-3 text-sm ${
-            theme == 'light' ?
-                'bg-rose-200 rounded-tr-none rounded-br-none text-rose-900' :
-                'bg-rose-950/80 border-[0.1px] border-r-0 rounded-tr-none rounded-br-none border-red-900/95 text-rose-300'
-        }`}
-            initial={{
-                translateX: '100%'
-            }}
-            animate={{
-                translateX: errors.size != 0 ? '10%' : '100%'
-            }}
-        >
-            <div className="flex items-center gap-4">
-                <IoIosCloseCircle className="text-xl text-rose-500" />
-                <p className="font-bold w-[90%]">Você teve {errors.size} erro(s) na aplicação</p>
-            </div>
-            {errors.size != 0 && <ul className="list-disc w-[90%] flex flex-col">
-                {[...errors.entries()].map(([key, msg]) => (
-                    <li key={key} className={`ml-15 ${theme == 'light' && 'text-rose-700'}`}>{msg}</li>
-                ))}
-            </ul> }
-        </motion.div>
-
-        <motion.div className={`fixed top-40 right-0 w-[30vw] rounded-sm p-2 flex flex-col gap-3 text-sm ${
-            theme == 'light' ?
-                'bg-emerald-200 rounded-tr-none rounded-br-none text-emerald-900' :
-                'bg-emerald-950/80 border-[0.1px] border-r-0 rounded-tr-none rounded-br-none border-emerald-900/95 text-emerald-300'
-        }`}
-            initial={{
-                translateX: '100%'
-            }}
-            animate={{
-                translateX: successes.size != 0 ? '10%' : '100%'
-            }}
-        >
-            <div className="flex items-center gap-4">
-                <IoIosCloseCircle className="text-xl text-emerald-500" />
-                <p className="font-bold w-[90%]">{successes.size} operação(ões) bem sucedida(s)</p>
-            </div>
-            {successes.size != 0 && <ul className="list-disc w-[90%] flex flex-col">
-                {[...successes.entries()].map(([key, msg]) => (
-                    <li key={key} className={`ml-15 ${theme == 'light' && 'text-emerald-700'}`}>{msg}</li>
-                ))}
-            </ul> }
-        </motion.div>
-    </>
+    return <div className="fixed bottom-10 left-1/2 -translate-x-1/2 flex flex-col gap-2 w-[90%] md:w-[30vw]">
+        <AnimatePresence>
+            {errors.size != 0 && <motion.div className={`rounded-sm p-2 flex flex-col gap-3 text-sm ${
+                theme == 'light' ?
+                    'bg-rose-200 text-rose-900' :
+                    'bg-rose-950/80 border-[0.1px] border-r-0 border-red-900/95 text-rose-300'
+            }`}
+                initial={{
+                    translateY: '0%',
+                    opacity: 0
+                }}
+                animate={{
+                    translateY: '-10%',
+                    opacity: 1
+                }}
+                exit={{
+                    opacity: 0,
+                    translateY: '0%'
+                }}
+            >
+                <div className="flex items-center gap-4">
+                    <IoIosCloseCircle className="text-xl text-rose-500" />
+                    <p className="font-bold w-[90%]">Você teve {errors.size} erro(s) na aplicação</p>
+                </div>
+                <ul className="list-disc w-[90%] flex flex-col">
+                    {[...errors.entries()].map(([key, msg]) => (
+                        <li key={key} className={`ml-15 ${theme == 'light' && 'text-rose-700'}`}>{msg}</li>
+                    ))}
+                </ul>
+            </motion.div>}
+        </AnimatePresence>
+        
+        <AnimatePresence>
+            {successes.size != 0 && <motion.div className={`rounded-sm p-2 flex flex-col gap-3 text-sm ${
+                theme == 'light' ?
+                    'bg-emerald-200 text-emerald-900' :
+                    'bg-emerald-950/80 border-[0.1px] border-r-0 border-emerald-900/95 text-emerald-300'
+            }`}
+                initial={{
+                    translateY: '0%',
+                    opacity: 0
+                }}
+                animate={{
+                    translateY: '-10%',
+                    opacity: 1
+                }}
+                exit={{
+                    opacity: 0,
+                    translateY: '0%'
+                }}
+            >
+                <div className="flex items-center gap-4">
+                    <IoIosCloseCircle className="text-xl text-emerald-500" />
+                    <p className="font-bold w-[90%]">{successes.size} operação(ões) bem sucedida(s)</p>
+                </div>
+                <ul className="list-disc w-[90%] flex flex-col">
+                    {[...successes.entries()].map(([key, msg]) => (
+                        <li key={key} className={`ml-15 ${theme == 'light' && 'text-emerald-700'}`}>{msg}</li>
+                    ))}
+                </ul>
+            </motion.div>}
+        </AnimatePresence>
+    </div>
 }
