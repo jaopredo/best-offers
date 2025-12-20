a0184f219c82c62359af77b6adcf39cb32b3e51a
jaopredo <joao.profissional.45@gmail.com>
2025-12-20 13:17:35 -0300

    Serviços de Job e Scrapping tipados

---
+++ frontend/types/api/services/job.service.ts
@@ -0,0 +1,54 @@
+import { AxiosResponse } from "axios"
+import { ApiDefaultResponse, ApiPaginationResponse, Pagination } from ".."
+import { APISourceInterface } from "../source"
+
+import { Font } from "./font.service"
+import { Category } from "./category.service"
+
+/* ENUM DOS STATUS DO JOB */
+export enum JobStatusEnum {
+    RUNNING = 'running',
+    FAILED = 'failed',
+    SUCCESS = 'success'
+}
+
+/* TIPO DO JOB */
+export type Job = {
+    id: number
+    status: JobStatusEnum
+    font: Font
+    category: Category
+}
+
+/* RESPOSTA PADRÃO DAS ROTAS DO JOB */
+export type JobApiResponse = ApiDefaultResponse<{ job: Job }>
+
+/* TIPAGEM DA PAGINAÇÃO DOS JOBS */
+export type JobPagination = Pagination & Partial<{ status: JobStatusEnum }>
+
+
+/* INTERFACE DO SERVIÇO DOS JOBS */
+export interface JobServiceInterface {
+    source: APISourceInterface
+
+    /**
+     * Função para recuperar uma lista com todos os jobs no sistema
+     * @param {Pagination} pagination - Objeto com as informações de paginação
+     * @returns {Promise<ApiPaginationResponse<Job>>} - A paginação dos jobs presentes no banco
+     */
+    getAll(pagination?: Pagination): Promise<ApiPaginationResponse<Job>>
+
+    /**
+     * Função para recuperar um job em específico
+     * @param {number} id - ID de um job específico
+     * @returns {Promise<AxiosResponse<Job>>} - O job especificado
+     */
+    get(id: number): Promise<AxiosResponse<Job>>
+
+    /**
+     * Deleta um job baseado no ID passado
+     * @param {number} id - ID do job que será deletado
+     * @returns {Promise<JobApiResponse>} - Promise com a resposta da requisição
+     */
+    delete: (id: number) => Promise<JobApiResponse>
+}
---
+++ frontend/types/api/services/scrapping.service.ts
@@ -0,0 +1,32 @@
+import { ApiDefaultResponse } from ".."
+import { APISourceInterface } from "../source"
+
+import { FontRegisterInterface } from "./font.service"
+import { AdapterRegisterInterface } from "./adapter.service"
+import { Job } from "./job.service"
+
+
+/* INTERFACE DO REGISTRO DE UM SCRAPPING */
+export type ScrappingRegisterInterface = ({
+    font: Omit<FontRegisterInterface, 'adapterId'>,
+    adapter: AdapterRegisterInterface
+})|({
+    fontId: number
+})
+
+/* RESPOSTA PADRÃO DAS ROTAS DO SCRAPPING */
+export type ScrappingApiResponse = ApiDefaultResponse<{ jobs: Job[] }>
+
+/* INTERFACE DO SERVIÇO DOS SCRAPPINGS */
+export interface ScrappingServiceInterface {
+    source: APISourceInterface
+
+    /* MÉTODOS BÁSICOS */
+    /**
+     * Método para realizar o registro de um scrapping no sistema (Cria vários jobs para realizar o scrapping de acordo
+     * com as informações passadas no corpo da requisição)
+     * @param {ScrappingRegisterInterface} data - As informações do novo scrapping
+     * @returns {Promise<ScrappingApiResponse>} - Promise com a resposta da requisição
+     */
+    create (data: ScrappingRegisterInterface): Promise<ScrappingApiResponse>
+}
